b217ede25685fec4575a99d3ff7e9c541b3b5353
Wanes007 <127496806+Wanes007@users.noreply.github.com>
2023-04-30 21:07:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,19 @@ void task_2(){
     writeToFile("result_task2.txt",convertDecToBin(25));
 }
 
+
+void task_3(){
+  srand(time(nullptr));
+  char binNum[65] = "";
+  int j = rand() % 64;
+  for(int i = 0; i < j; ++i)
+    binNum[i] = rand() % 2 + '0';
+  char* hexNum = convertBinToHex(binNum);
+  writeToFile("result_task3.txt", true, hexNum, binNum);
+  delete[] hexNum;
+}
+
+
 void task_4(){
     buildTree (5);
 }
